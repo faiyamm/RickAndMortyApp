2605b52fa3c561c5803b2f0adc6d150d11461c7d
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-02-20 21:46:14 -0800

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Fai on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
